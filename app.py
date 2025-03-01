a0c673db09667878566d1006a1742f04acc5e3d7
TO-spica <takoashi_0725@yahoo.co.jp>
2025-03-01 13:10:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ import time
 # 取得対象のURLと測定者をリストにする
 src = []
 #src.append(["Toshi@横浜 さん (神奈川県横浜市青葉区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0101"])
-src.append(["はるわんだ さん (横浜市青葉区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0102"])
+#src.append(["はるわんだ さん (横浜市青葉区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0102"])
 #src.append(["ノブ さん (神奈川県大和市中央林間西)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0110"]) 
 #src.append(["Masahiro さん (横浜市都筑区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0100"]) 
 #src.append(["abe さん (東京都町田市森野)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0088"]) 
 #src.append(["パライラル さん (東京都町田市成瀬台)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0089"]) 
 #src.append(["ぽんぽん さん (町田市金井)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0091"]) 
+src.append(["テスト", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0126"]) 
 
 # 表記ゆれをなくすため、都県を削除
 for i in range(len(src)):
