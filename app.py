b4e7a6d0b137d8eaa730465d3a74284228708304
TO-spica <takoashi_0725@yahoo.co.jp>
2025-03-01 13:12:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ import time
 
 # 取得対象のURLと測定者をリストにする
 src = []
-#src.append(["Toshi@横浜 さん (神奈川県横浜市青葉区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0101"])
-#src.append(["はるわんだ さん (横浜市青葉区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0102"])
-#src.append(["ノブ さん (神奈川県大和市中央林間西)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0110"]) 
-#src.append(["Masahiro さん (横浜市都筑区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0100"]) 
-#src.append(["abe さん (東京都町田市森野)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0088"]) 
-#src.append(["パライラル さん (東京都町田市成瀬台)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0089"]) 
-#src.append(["ぽんぽん さん (町田市金井)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0091"]) 
-src.append(["テスト", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0126"]) 
+src.append(["Toshi@横浜 さん (神奈川県横浜市青葉区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0101"])
+src.append(["はるわんだ さん (横浜市青葉区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0102"])
+src.append(["ノブ さん (神奈川県大和市中央林間西)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0110"]) 
+src.append(["Masahiro さん (横浜市都筑区)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0100"]) 
+src.append(["abe さん (東京都町田市森野)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0088"]) 
+src.append(["パライラル さん (東京都町田市成瀬台)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0089"]) 
+src.append(["ぽんぽん さん (町田市金井)", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0091"]) 
+#src.append(["テスト", "https://weathernews.jp/pollen/pollen.html?pid=POLLEN_2025-0126"]) 
 
 # 表記ゆれをなくすため、都県を削除
 for i in range(len(src)):
@@ -29,7 +29,7 @@ for i in range(len(src)):
 # という形にする。
 
 dtnow = datetime.datetime.now()
-st.write("現在時刻：" + dtnow.strftime("%Y/%m/%d"))
+st.write("現在時刻：" + dtnow.strftime("%Y/%m/%d %T"))
 
 for j in range(1):
   dt = dtnow + datetime.timedelta(days = -j) + datetime.timedelta(hours = +9)
@@ -40,7 +40,7 @@ for j in range(1):
     if match:
       url = "https://site.weathernews.jp/site/pollen/json/obs/" + dt.strftime("%Y/%m/%d") + "/" + match.group() + ".json"
       uri.append(url)
-  st.write(uri)
+  #st.write(uri)
   
   # 各URIを使って花粉(個)のデータを取得する。
   kafun = []
@@ -50,7 +50,7 @@ for j in range(1):
     response.encoding = response.apparent_encoding  # 文字エンコーディングを適切に設定
     # ① JSONデータを直接取得
     json_data = response.json()  # decode() は不要
-    st.write(json_data)
+    #st.write(json_data)
     # ② 必要なデータ（pollenの配列）を取得
     recv = json_data["obs"]["pollen"]
     kafun.append([list(range(1,len(recv)+1)),recv])
@@ -62,4 +62,4 @@ for j in range(1):
     fig.add_trace(go.Scatter(x=kafun[i][0], y=kafun[i][1], name=src[i][0]))
   st.plotly_chart(fig)
 
-st.write(kafun)
+#st.write(kafun)
